15212ff5ff8b626a4978ab239fb697def7794056
Kariuki Antony <antonygichoya9@gmail.com>
2023-11-16 19:29:57 +0300

    work on docker file

--- .flaskenv
+++ .flaskenv
@@ -1,5 +1,3 @@
 export FLASK_ENV=development
 export FLASK_DEBUG=True
 export FLASK_APP=src
-S_D_U = sqlite:///bookmarks.db
-S_T_M = False
--- src/__init__.py
+++ src/__init__.py
@@ -11,10 +11,11 @@ from src.config.swagger import template, swagger_config
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    print(os.getenv("S_D_U"))
     if test_config is None:
         app.config.from_mapping(SECRET_KEY=os.environ.get("SECRET_KEY"),
                                 SQLALCHEMY_DATABASE_URI =os.environ.get("S_D_U"),
-                                SQLALCHEMY_TRACK_MODIFICATION = False,
+                                SQLALCHEMY_TRACK_MODIFICATIONS = False,
                                 JWT_SECRET_KEY = os.environ.get("jwt_key"),
                                 SWAGGER={"title": "Bookmarks API", 
                                          "uiversion": 3}
